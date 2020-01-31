023ddbc5558c47efeb8ec88c63b6308a1891f849
casallasmo <casallas@gmail.com>
2020-01-30 21:43:30 -0500

    mensaje

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,6 @@ public class helloworld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window. mi nombre es marco casallas
-        System.out.println("marco , chaos world");
     }
 
 }
